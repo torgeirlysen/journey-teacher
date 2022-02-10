0ceedefee7bcad00c5bb2e149a483ffbb6f2d50f
DanielOdichoErHomo <32740390+DanielOdichoErHomo@users.noreply.github.com>
2022-02-10 12:35:39 +0100

    changed homepage in package json

---
+++ precache-manifest.54b3a8a534be93f9faec958d2ade3559.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7b81321ce91b07c9628c95368ea4810",
+    "url": "/journey-teacher/index.html"
+  },
+  {
+    "revision": "5c6c7f738247b74f07a4",
+    "url": "/journey-teacher/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "e4ddcf19f20ca494e285",
+    "url": "/journey-teacher/static/js/2.62eae81d.chunk.js"
+  },
+  {
+    "revision": "2ed70a1a2df1f93a1f29",
+    "url": "/journey-teacher/static/js/3.9209433f.chunk.js"
+  },
+  {
+    "revision": "5c6c7f738247b74f07a4",
+    "url": "/journey-teacher/static/js/main.681e9589.chunk.js"
+  },
+  {
+    "revision": "bed8a777a352a3807d8f",
+    "url": "/journey-teacher/static/js/runtime~main.9f16287c.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/journey-teacher/static/media/logo.06e73328.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c883a7754df930df59479cdf183aab00.js"
+  "/journey-teacher/precache-manifest.54b3a8a534be93f9faec958d2ade3559.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/journey-teacher/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
