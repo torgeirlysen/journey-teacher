5949903130bf0bf964c07672067cb86a26673791
DanielOdichoErHomo <32740390+DanielOdichoErHomo@users.noreply.github.com>
2022-02-10 01:08:30 +0100

    sdfsdf

---
+++ precache-manifest.c883a7754df930df59479cdf183aab00.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f338c1404c17af4965893041ec1047c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5c6c7f738247b74f07a4",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "e4ddcf19f20ca494e285",
+    "url": "/static/js/2.62eae81d.chunk.js"
+  },
+  {
+    "revision": "2ed70a1a2df1f93a1f29",
+    "url": "/static/js/3.9209433f.chunk.js"
+  },
+  {
+    "revision": "5c6c7f738247b74f07a4",
+    "url": "/static/js/main.681e9589.chunk.js"
+  },
+  {
+    "revision": "4713837f6ac13d23c5bc",
+    "url": "/static/js/runtime~main.fa905a2c.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/static/media/logo.06e73328.svg"
+  }
+]);
